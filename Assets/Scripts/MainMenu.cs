be3f3df72587330a8d632b9eb9ac9a61c13de256
mfikindlovu8-ui <mfiki.ndlovu8@gmail.com>
2026-08-23 16:19:37 +0200

    ui

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,20 +5,38 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject MainMenuPanel;
     public GameObject settingsPanel;
+    public GameObject controlsPanel;
+
     public void OpenSettings()
     {
         MainMenuPanel.SetActive(false);
         settingsPanel.SetActive(true);
+        controlsPanel.SetActive(false);
     }
 
     public void CloseSettings()
     {
         settingsPanel.SetActive(false);
+        controlsPanel.SetActive(false);
         MainMenuPanel.SetActive(true);
     }
+
+    public void OpenControls()
+    {
+        MainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        controlsPanel.SetActive(true);
+    }
+
+    public void CloseControls()
+    {
+        controlsPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("Main Scene Prototype");
     }
 
     public void QuitGame()
@@ -27,4 +45,3 @@ public class MainMenu : MonoBehaviour
     }
 }
 
-     
